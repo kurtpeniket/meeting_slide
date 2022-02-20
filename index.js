286ab024b3a657638274ec7cf50d5d992c140e91
Kurt Peniket <kurt@ggapps.co.uk>
2022-02-20 18:08:28 +0000

    js changes

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const today = new Date().getDay()
+
 function toggleFullscreen(event) {
   let element = document.body;
 
@@ -17,50 +19,47 @@ function toggleFullscreen(event) {
   isFullscreen ? document.cancelFullScreen() : element.requestFullScreen();
 }
 
-function msToTime(s) {
-  //let seconds = (ms / 1000).toFixed(1);
-  //let minutes = (ms / (1000 * 60)).toFixed(1);
-  //let hours = (ms / (1000 * 60 * 60)).toFixed(1);
-  //let days = (ms / (1000 * 60 * 60 * 24)).toFixed(1);
-  //return hours + minutes + seconds;
-  //if (seconds < 60) return seconds + " Sec";
-  //else if (minutes < 60) return minutes + " Min";
-  //else if (hours < 24) return hours + " Hrs";
-  //else return days + " Days"
-  if (s > 0) {
-    var ms = s % 1000;
-    s = (s - ms) / 1000;
-    var secs = s % 60;
-    s = (s - secs) / 60;
-    var mins = s % 60;
-    var hrs = (s - mins) / 60;
+function formatNums(n) {
+  return n < 10 ? `0${n}` : n
+}
 
-    return hrs + ':' + mins + ':' + secs
+function msToTime(s) {
+  if (s > 0) {
+    let ms = s % 1000;
+    s = (s - ms) / 1000;
+    let secs = s % 60;
+    s = (s - secs) / 60;
+    let mins = s % 60;
+    //let hrs = (s - mins) / 60;
+
+    return formatNums(mins) + ':' + formatNums(secs)
   } else {
     return "00:00:00"
   }
 }
 
-// getDay() returns the day of the week for the specified date according to local time, where 0 represents Sunday.
+// getDay() returns the day of the week for the specified date according to local time, where 0 represents Sunday
 function getTimeDiff(day, time) {
   let dt = new Date();
   while (dt.getDay() != day) {
     dt.setDate(dt.getDate() - day);
   }
 
-  time == "am" ? dt.setHours(10, 0, 0) : dt.setHours(19, 0, 0)
+  time == "am" ? dt.setHours(10, 0, 0) : dt.setHours(18, 10, 0)
 
   let difference = Date.parse(dt) - Date.now()
-  return msToTime(difference);
+  return difference;
 }
 
 function displayClock(){
-  let display = getTimeDiff(0, "am");
-  document.getElementById('timer').innerHTML = display;
+  let display = null
+  today == 0 ? display = getTimeDiff(today, "pm") : display = getTimeDiff(today, "pm");
+  document.getElementById('timer').innerHTML = msToTime(display);
+  if (display < 120000) {
+    document.getElementById('timer').style.color = 'red'
+  }
   setTimeout(displayClock, 1000);
 }
 
 window.onload = displayClock();
 
-// TODO - select meeting day on view
-// TODO - change colour when < 2mins?
